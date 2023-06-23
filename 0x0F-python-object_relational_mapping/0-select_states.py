7b0cbeb981b0f92a107657b13ce50db6e9671068
MSI <wyeabkal@gmail.com>
2023-06-23 09:32:12 +0300

    adjusted

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,16 +8,14 @@ username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=username,
-                     database=database, port=3306,
-                     passwd=password)
-
-cur = db.cursor()
-query = "SELECT * FROM states ORDER BY id ASC"
-cur.execute(query)
-
-for i in cur:
-    print(i)
-
 if __name__ == '__main__':
-    pass
+    db = MySQLdb.connect(host='localhost', user=username,
+                        database=database, port=3306,
+                        passwd=password)
+
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query)
+
+    for i in cur:
+        print(i)
