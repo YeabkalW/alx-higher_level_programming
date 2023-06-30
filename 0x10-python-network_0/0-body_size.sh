cc94fed495351fdb2dbcf3b6806219861749cfb9
MSI <wyeabkal@gmail.com>
2023-06-30 15:35:07 +0300

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # a bash script that takes url and send request to display the
 # the size of the body of the response.
+url="$1"
 
-curl -sI $1 | awk '/Content-Length/ (print $2)'
+# Send request using curl, retrieve only the headers, and store in a variable
+headers=$(curl -sI "$url")
+
+# Extract the Content-Length header value
+content_length=$(echo "$headers" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
+
+# Display the size of the body in bytes
+echo "$content_length"
